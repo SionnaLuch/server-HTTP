141a5f264c181033bb843352176d9e3c4a2e35a4
Sionna Poleszuk <sionna.pol@gmail.com>
2022-03-17 21:24:40 +0100

    27.4 contact form

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({storage: storage})
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.post('/contact/send-message', upload.single('uploadedFile'), (req, res) => {
+    const { author, sender, title, message } = req.body;
+    const uploadedFile = req.file;
+    if (author && sender && title && message && uploadedFile) {
+      res.render('contact', { isSent: true, name: uploadedFile.originalname });
+    }
+    else {
+      res.render('contact', { isError: true });
+    }
+  });
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
@@ -34,7 +55,8 @@ app.get('/history', (req, res) => {
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-})
+});
+
 
 app.listen(8008, () => {
   console.log('Server is running on port: 8008');
